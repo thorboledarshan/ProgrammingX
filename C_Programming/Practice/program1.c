49879a5173682ad92d1424686f6d812dc4f973e9
Darshan <thorboledarshan@gmail.com>
2026-05-31 01:16:01 +0530

    ALL

---
+++ C_Programming/Practice/program1.c
@@ -0,0 +1,15 @@
+/////////////////////////////
+// Marvellous Infosystem 
+////////////////////////////
+
+#include<stdio.h>
+int main()
+{
+    printf("Jay Ganesh....\n");
+    return 0;
+}
+
+/*
+    "आयुष्य खूप सुंदर आहे... फक्त coding🖥️ जमली पाहिजे"
+                                        -Piyush Sir 
+*/
