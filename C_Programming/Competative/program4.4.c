51ecc9c746396e3ed2ad7aa14ca94d54ac48fb9c
Darshan <thorboledarshan@gmail.com>
2026-06-01 20:28:47 +0530

    Time Complexity

---
+++ C_Programming/Competative/program4.4.c
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////
+//
+// Q.Write a program which accept number from user  and return summation  
+//   of all its non-factors
+//
+////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+int SumNonFact(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+    for(iCnt =1 ;iCnt <= iNo; iCnt++)
+    {
+        if(iNo % iCnt != 0)
+        {
+          iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+    
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+
+    iRet = SumNonFact(iValue);
+    printf("summation of non factors is : %d\n",iRet);
+    return 0;
+}
+
+///////////////////////////
+//
+// input : 12
+// output: 50
+//
+/////////////////////////
