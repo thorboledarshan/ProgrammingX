057c6a1480ea3e2ee5609cfb52bbee3654cfb397
Darshan <thorboledarshan@gmail.com>
2026-06-02 23:15:10 +0530

    Time Complexity

---
+++ C_Programming/Competative/program5.1.c
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////////
+//
+// Q.Write a program which accept number from user  and print that number  
+//   of $ and * on screen
+//
+////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    if(iNo < 0 )
+    {
+        iNo = -iNo;
+    }
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("& * ",iCnt);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter number:");
+    scanf("%d",&iValue);
+    Pattern(iValue);
+    return 0;
+}
+
+///////////////////////////
+//
+// input : -3
+// output: & * & * & *
+//
+/////////////////////////
