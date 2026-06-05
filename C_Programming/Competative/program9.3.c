60f32cfe79b9d3445b9a01b657338a9b28c47607
Darshan <thorboledarshan@gmail.com>
2026-06-05 23:54:28 +0530

    Codes

---
+++ C_Programming/Competative/program9.3.c
@@ -0,0 +1,46 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. write a program which accept number from user and return the count of  digit
+//    between 3 and 7
+///////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+int CountRange(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+    int iCount = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    while(iNo !=0)
+    {
+        iDigit = iNo % 10;
+        iNo = iNo / 10;
+        if((iDigit > 3)&&(iDigit < 7))
+        {
+            iCount = iCount + 1;
+        }
+    }
+    return iCount;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number:");
+    scanf("%d",&iValue);
+
+    iRet = CountRange(iValue);
+    printf("Count of Range Digits are : %d",iRet);
+    return 0;
+}
+
+//////////////////////////////////////
+//
+// input : 2395
+// output: 1
+//
+//////////////////////////////////////
