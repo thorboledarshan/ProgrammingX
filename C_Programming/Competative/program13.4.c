a3b6c0d74fe19c01db8f675d836c385602a9e39f
Darshan <thorboledarshan@gmail.com>
2026-06-07 18:46:06 +0530

    C code

---
+++ C_Programming/Competative/program13.4.c
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. Accept N numbers from user and display all such elements  which are divisible by 3 
+//     and divisible by 5
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display(int Arr[], int iLength)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if((Arr[iCnt] % 3 == 0) && (Arr[iCnt] % 5 == 0))
+        {
+           printf("%d ",Arr[iCnt]);
+        }
+    }
+
+}
+int main()
+{
+    int iSize = 0;
+    int iCnt = 0;
+    int *P = NULL;
+
+    printf("Enter number of elements:");
+    scanf("%d",&iSize);
+
+    P = (int *)malloc(iSize * sizeof(int));  //memory allocation
+
+    if(P == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ", iCnt+1);
+        scanf("%d",&P[iCnt]);
+    }
+
+    Display(P,iSize);
+
+    free(P); //De-allocate memory
+
+    return 0;
+}
+
+//////////////////////////////////////
+//
+// input : Enter number of elements: 3
+//         Enter element 1 : 10
+//         Enter element 2 : 20
+//         Enter element 3 : 30
+// output: 30
+//
+//////////////////////////////////////
