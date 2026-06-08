935bcfe30a8f1a09563f591f8562d8dd5b30b35a
Darshan <thorboledarshan@gmail.com>
2026-06-08 22:37:07 +0530

    P

---
+++ C_Programming/Practice/program6.c
@@ -0,0 +1,25 @@
+/*
+   (Algorithm)
+   START
+        Accept first number as No1
+        Accept second number as No2
+        perform addition of No1 and No2
+        Display the result 
+   STOP
+*/
+#include<stdio.h>
+int main()
+{
+    float i=0.0f,j=0.0f,k=0.0f; //Variable creation with default values
+
+    printf("Enter first number :\n");
+    scanf("%f",&i);
+
+    printf("Enter second number :\n");
+    scanf("%f",&j);
+
+    k = i + j;
+    printf("Addition is: %f\n",k);
+    
+    return 0;
+}
