1a3729dcd76e04ec63875783f09b50b11bcb07bb
Darshan <thorboledarshan@gmail.com>
2026-06-09 23:15:50 +0530

    CC

---
+++ C_Programming/Competative/program15.3.c
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. Accept N numbers from user and accept one another number as No, and return index of
+//    last occurence of that number.
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+
+int LastOccurence(int Arr[], int iLength, int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = (iLength-1); iCnt >= 0; iCnt--)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            return iCnt;
+            break;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int iSize = 0;
+    int iValue = 0;
+    int * P = NULL;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter number  elements:");
+    scanf("%d",&iSize);
+
+    P = (int *) malloc (iSize * sizeof(int));
+
+    if(P == NULL)
+    {
+        return -1;
+    }
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt+1);
+        scanf("%d",&P[iCnt]);
+    }
+
+    printf("Enter number to check : ");
+    scanf("%d",&iValue);
+
+    iRet = LastOccurence(P, iSize, iValue);
+    printf("Index of last occurence is : %d",iRet);
+
+    free(P);
+
+    return 0;
+
+    
+    
+}
+
+//////////////////////////////////////
+//
+// input : Enter number of elements: 3
+//         Enter element 1 : 10
+//         Enter element 2 : 11
+//         Enter element 3 : 11
+//         Enter number to check: 11
+// output: Index of first occurence is : 1
+//
+//////////////////////////////////////
