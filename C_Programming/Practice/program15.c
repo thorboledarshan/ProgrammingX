7c5241df4dfe531a17c521433dca183545e992f4
Darshan <thorboledarshan@gmail.com>
2026-06-09 23:21:40 +0530

    P

---
+++ C_Programming/Practice/program15.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int CheckEvenOdd(int iNo)
+{
+    int iReminder=0;
+    iReminder = iNo %2;
+    if(iReminder ==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue=0;
+    bool bRet=false;
+
+    printf("Enter number to check even or odd number:\n");
+    scanf("%d",&iValue);
+
+    bRet =CheckEvenOdd(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is even\n",iValue);
+    }
+    else
+    {
+        printf("%d is odd\n",iValue);
+    }
+    return 0;
+
+}
