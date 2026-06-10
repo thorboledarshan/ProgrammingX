b04a91427a1ecc591b119dae9eab442a9149989a
Darshan <thorboledarshan@gmail.com>
2026-06-10 23:15:58 +0530

    CP

---
+++ C_Programming/Competative/16.3DiffMaxMin.c
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. Accept N numbers from user and return the difference between largest and smallest number.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+
+int DiffMaxMin(int Arr[], int iLength)
+{
+    int iCnt = 0;
+    int iMin =Arr[0];
+    int iMax = 0;
+    int iDiff = 0;
+
+    for(iCnt = 1; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] < iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+        else if (Arr[iCnt] > iMax)
+        {
+            iMax = Arr[iCnt];
+        }
+    }
+    iDiff = iMax - iMin;
+    return iDiff;
+}
+int main()
+{
+    int iSize = 0;
+    int iCnt = 0;
+    int * P = NULL;
+    int iRet = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d",&iSize);
+
+    P = (int *) malloc (iSize * sizeof(int));
+
+    if(P ==NULL)
+    {
+        return -1;
+    }
+
+    for(iCnt  = 0;iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt+1);
+        scanf("%d",&P[iCnt]);
+    }
+    
+    iRet = DiffMaxMin(P, iSize);
+    printf("Difference between max and min number is : %d ",iRet);
+    free(P);
+    return 0;
+}
+//////////////////////////////////////////////////////////////
+//
+// input : Enter number of elements: 3
+//         Enter element 1 : 10
+//         Enter element 2 : 20
+//         Enter element 3 : 30
+// output: Difference between max and min number is : 20 
+//
+///////////////////////////////////////////////////////////////
