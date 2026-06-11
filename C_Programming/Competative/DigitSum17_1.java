cc58741c51faba86f5e703094a1c2975d26af34a
Darshan <thorboledarshan@gmail.com>
2026-06-11 23:11:34 +0530

    CP

---
+++ C_Programming/Competative/DigitSum17_1.java
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. Find the sum of Digits of given number.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Logic
+{
+    void SumOfDigits(int num)
+    {
+      int iSum = 0;
+      int iDigit = 0; 
+      int iTemp = num;
+
+      while(num != 0)
+      {
+        iDigit = num % 10;
+        iSum = iSum + iDigit;
+        num = num /10;
+      }
+      System.out.println("Sum of Digit "+iTemp+" is : "+iSum);
+    }
+
+}
+
+
+class DigitSum17_1
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.SumOfDigits(1234);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// input : 1234
+// ouput : Sum of Digit 1234 is : 10
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
