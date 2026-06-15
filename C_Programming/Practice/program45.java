b9a8e174d8a08dc115c85a715d73631b676dd73b
Darshan <thorboledarshan@gmail.com>
2026-06-15 23:14:07 +0530

    PC

---
+++ C_Programming/Practice/program45.java
@@ -0,0 +1,45 @@
+//Type 2
+
+import java.util.Scanner;
+
+class NumberX
+{
+  public static boolean CheckDivisbile(int iNo)
+    {
+         if((iNo %3 == 0) && (iNo %5 == 0))
+        {
+          return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+class program45
+{
+
+    
+    public static void main(String A[])
+    {
+      int iValue = 0;
+      boolean bRet = false;
+
+      Scanner sobj = new Scanner(System.in);
+      System.out.println("Enter number:");
+      iValue = sobj.nextInt();
+
+      NumberX nobj = new NumberX();   
+      bRet =nobj.CheckDivisbile(iValue);
+
+      if(bRet == true)
+      {
+        System.out.println("Number is divisible by 3&5");
+      }
+      else
+      {
+        System.out.println("Number is  not divisible by 3&5");
+      }
+
+    }
+}
