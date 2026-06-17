0f62382d5e937da8f42eae14e543c9b88f4b9014
Darshan <thorboledarshan@gmail.com>
2026-06-17 23:11:55 +0530

    PC

---
+++ C_Programming/Practice/program57.c
@@ -0,0 +1,24 @@
+//input : 5
+//output : 1 3 5 
+#include<stdio.h>
+
+void Display(int iNo)
+{
+  int iCnt=0;
+  for(iCnt=1; iCnt<=iNo; iCnt +=2)
+  {
+    printf("%d\t",iCnt);
+  }
+  printf("\n");
+  
+}
+int main()
+{
+    int iValue=0;
+    printf("Enter a number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
