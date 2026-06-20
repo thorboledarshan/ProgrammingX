a3625321ffcfd349b3267738473bba9d06b29f34
Darshan <thorboledarshan@gmail.com>
2026-06-20 19:23:33 +0530

    PC

---
+++ C_Programming/Practice/program73.c
@@ -0,0 +1,17 @@
+//input : 5
+// output: 12345
+#include<stdio.h>
+
+int main()
+{
+   int iCnt=0;
+    iCnt=1;
+    while(iCnt<=5)
+    {
+        printf("%d \t",iCnt);
+        iCnt++;
+    }
+    printf("\n");
+
+   return 0;
+}
