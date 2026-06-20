4af85d63b4b9cc338075740f1c0bb89e30bdabe0
Darshan <thorboledarshan@gmail.com>
2026-06-20 19:23:09 +0530

    PC

---
+++ C_Programming/Practice/program72.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt=0;
+    iCnt=1;
+    do
+    {
+        printf("Jay ganesh...\n");
+        iCnt++;
+    }while(iCnt <=5);
+    
+
+    return 0;
+}
