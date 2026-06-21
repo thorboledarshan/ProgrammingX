e706f4367311c2bcf0970c28944d2060e46165ca
Darshan <thorboledarshan@gmail.com>
2026-06-21 23:32:03 +0530

    PC

---
+++ C_Programming/Practice/program76.c
@@ -0,0 +1,20 @@
+//input : 5
+// output: 5 4 3 2 1
+#include<stdio.h>
+
+int main()
+{
+    int iNo = 751;
+    int iDigit = 0;
+
+    while(iNo !=0)
+    {
+      iDigit = iNo % 10;
+      printf("%d\n",iDigit);
+      iNo = iNo/10;
+    }
+
+    
+    
+   return 0;
+}
