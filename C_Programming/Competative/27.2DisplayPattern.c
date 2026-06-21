01675178ef3d7b243d6292e3e2623931315fa484
Darshan <thorboledarshan@gmail.com>
2026-06-21 23:29:38 +0530

    CP

---
+++ C_Programming/Competative/27.2DisplayPattern.c
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. Accept number of rows and number of columns  and display pattern
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void DisplayPattern(int iRow, int iCol)
+{
+  int i = 0;
+  int j = 0;  
+
+  for(i = iRow; i >= 1; i--)
+  {
+    for(j = 1; j <= iCol; j++)
+    {
+      if(j == 1 || i >= j || i == iRow )
+      {
+        printf("*  ");
+      }
+      else 
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+}
+int main()
+{
+  int iValue1 = 0;
+  int iValue2 = 0;
+
+  printf("Enter number of rows and columns :\n");
+  scanf("%d %d",&iValue1,&iValue2);
+
+  DisplayPattern(iValue1,iValue2);
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// input : Enter number of rows and columns : 4 ,4
+// output :  *   
+//           *  *    
+//           *  *  *  
+//           *  *  *  *
+//         
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
