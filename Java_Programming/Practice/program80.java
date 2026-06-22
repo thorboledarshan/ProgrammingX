053667166ce494bcbaaa3e52c69db5dc9414d586
Darshan <thorboledarshan@gmail.com>
2026-06-22 23:44:47 +0530

    PC

---
+++ Java_Programming/Practice/program80.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class DigitX
+{
+    public void DisplaydDigits(int iNo)
+    {
+        while(iNo !=0)
+        {
+            System.out.println(iNo % 10); 
+            iNo = iNo /10 ;
+        }
+        
+    }
+    
+
+
+}
+
+class program80
+{
+    public static void main (String A[])
+    {
+      Scanner sobj = new Scanner(System.in);
+      DigitX dobj = new DigitX();
+      
+      int iValue = 0;
+      System.out.println("Enter Number:");
+      iValue = sobj.nextInt();
+
+      dobj.DisplaydDigits(iValue);
+
+    }
+}
