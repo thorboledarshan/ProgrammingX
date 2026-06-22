c739ce925ab5210c4507e6f4fbd841b9f92f6c56
Darshan <thorboledarshan@gmail.com>
2026-06-22 23:42:46 +0530

    CP

---
+++ C_Programming/Competative/28.4DisplayPattern.c
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. Accept number of rows and number of columns  and display pattern
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void DisplayPattern(int iRow, int iCol)
+{
+  int i = 0;
+  int j = 0;  
+
+  for(i = iRow; i >= 1; i--)
+  {
+    for(j = 1; j <= iCol; j++)
+    {
+      if(j == i || i == iRow || j == iCol || j==1 || i==1)
+      {
+        printf("*  ");
+      }
+      else if((i>=j) != (j == i || i == iRow || j == iCol || j==1 || i==1)) 
+      {
+        printf("#  ");
+      }
+      else 
+      {
+        printf("$  ");
+      }
+    }
+    printf("\n");
+  }
+}
+int main()
+{
+  int iValue1 = 0;
+  int iValue2 = 0;
+
+  printf("Enter number of rows and columns :\n");
+  scanf("%d %d",&iValue1,&iValue2);
+
+  DisplayPattern(iValue1,iValue2);
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// input : Enter number of rows and columns : 4 ,4
+// output :  *  *  *  *
+//           *  #  *  *   
+//           *  *  $  *
+//           *  *  *  *
+//         
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
---
+++ C_Programming/Competative/28.5DisplayPattern.c
@@ -0,0 +1,55 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Q. Accept number of rows and number of columns  and display pattern
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void DisplayPattern(int iRow, int iCol)
+{
+  int i = 0;
+  int j = 0;  
+
+  for(i = 1; i <= iRow; i++)
+  {
+    for(j = 1; j <= iCol; j++)
+    {
+      if(j == i || i == iRow || j == iCol || j==1 || i==1)
+      {
+        printf("%d  ",j);
+      }
+      else if((i>=j) != (j == i || i == iRow || j == iCol || j==1 || i==1)) 
+      {
+        printf("   ");
+      }
+      else 
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+}
+int main()
+{
+  int iValue1 = 0;
+  int iValue2 = 0;
+
+  printf("Enter number of rows and columns :\n");
+  scanf("%d %d",&iValue1,&iValue2);
+
+  DisplayPattern(iValue1,iValue2);
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// input : Enter number of rows and columns : 4 ,4
+// output :  1  2  3  4  5
+//           1  2        5 
+//           1     3     5
+//           1        4  5
+//           1  2  3  4  5
+//         
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
