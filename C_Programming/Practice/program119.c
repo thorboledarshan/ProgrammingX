24157962c8b990fdbd40f94b5d8c8922aa00669e
Darshan <thorboledarshan@gmail.com>
2026-06-28 07:58:14 +0530

    PC

---
+++ C_Programming/Practice/program119.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//Error due to header stdlib.h
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    
+    //(STEP 1)Accept number of elements 
+    printf("Enter number of elements :\n");
+    scanf("%d",&iLength);
+    
+    //(STEP 2) Allocate the memory
+    Brr = (int *)malloc(iLength*sizeof(int)); //typecast
+
+    //(STEP 3) Accept the value from user
+    for(iCnt=0;iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    //(STEP 4) Use the memory(logic)
+
+
+    // (STEP 5) Deallocate the memory
+    free(Brr);
+
+    return 0;
+}
