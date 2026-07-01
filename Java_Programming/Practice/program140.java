fdfa409f120c9be9c7110dce3f4d6e0a5966bf2a
Darshan <thorboledarshan@gmail.com>
2026-07-01 09:00:19 +0530

    PC

---
+++ Java_Programming/Practice/program140.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class program140
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+        System.out.println(Arr.length);
+        
+        //Bad programming practice
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+
+        System.out.println(iCnt); //Error
+    }
+}
