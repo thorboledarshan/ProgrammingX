31cd9848e321db8132c655f5067d770bd9498746
Darshan <thorboledarshan@gmail.com>
2026-07-03 08:46:08 +0530

    PC

---
+++ C++_Programming/Practice/program148.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int Brr[] = {10,20,30,40,50};
+  
+  int iCnt = 0;
+  for(iCnt = 0; iCnt < 5; iCnt++)
+  {
+    printf("%d \n",Brr[iCnt]);
+  }
+  return 0;
+}
