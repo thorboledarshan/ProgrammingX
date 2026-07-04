da1f55caba1315c4892289ef0df22f29adfaa0f3
Darshan <thorboledarshan@gmail.com>
2026-07-04 22:05:08 +0530

    PC

---
+++ C++_Programming/Practice/program151.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+  //Accesss specifier of summation is PRIVATE
+ int Summation(int Arr[], int iSize)
+ {
+   int iCnt = 0;
+   int iSum = 0;
+
+   for(iCnt = 0; iCnt < iSize; iCnt++)
+   {
+     iSum = iSum + Arr[iCnt];
+   }
+   return iSum;
+
+ }
+};
+
+int main()
+{
+  int * Brr = NULL;
+  int iLength = 0, iCnt = 0, iRet = 0;
+  ArrayX aobj;
+
+  cout<<"Enter the number of elements : \n";
+  cin>>iLength;
+
+  Brr = new int[iLength];
+
+  cout<<"Enter the elements : \n";
+  for(iCnt = 0; iCnt < iLength; iCnt++)
+  {
+    cin>>Brr[iCnt];
+  }
+
+  cout<<"elements of the array are : \n";
+  for(iCnt = 0; iCnt < iLength; iCnt++)
+  {
+    cout<<Brr[iCnt]<<endl;
+  }
+
+  iRet = aobj.Summation(Brr,iLength);
+  cout<<"Summation is : "<<iRet<<endl;
+
+  delete []Brr;
+
+  return 0;
+}
---
+++ C++_Programming/Practice/program153.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+  public:
+    int *Arr;
+    int iSize;
+};
+
+int main()
+{
+  ArrayX aobj;
+  cout<<sizeof(aobj)<<endl; //16
+  return 0;
+}
