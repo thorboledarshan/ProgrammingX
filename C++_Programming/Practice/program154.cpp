b4aacc75043c217271468623be14cd94f067e88d
Darshan <thorboledarshan@gmail.com>
2026-07-04 22:05:39 +0530

    PC

---
+++ C++_Programming/Practice/program154.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#pragma pack(1)
+
+class ArrayX
+{
+  public:
+    int *Arr;
+    int iSize;
+};
+
+int main()
+{
+  ArrayX aobj;
+  cout<<sizeof(aobj)<<endl; //16
+  return 0;
+}
