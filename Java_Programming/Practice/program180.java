75b3660a97ca8ecb0b7224c9a47c6872b475a45d
Darshan <thorboledarshan@gmail.com>
2026-07-06 08:52:18 +0530

    PC

---
+++ Java_Programming/Practice/program180.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class program180
+{ 
+  public static void Display(int iNo)
+  {
+    int iCnt = 0;
+    char Ch = '\0';
+    for(iCnt = 1, Ch = 'a'; iCnt <= iNo; iCnt++, Ch++)
+    {
+      System.out.print(Ch+"\t");
+    }
+    System.out.println();
+  }
+
+  public static void main (String A[])
+  {
+    Scanner sobj = new Scanner(System.in);
+    int iValue = 0;
+
+    System.out.println("Enter number of elements : ");
+    iValue = sobj.nextInt();
+
+    Display(iValue);
+  }
+}
