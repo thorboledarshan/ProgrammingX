7ebf493e6e659be30f45c7da61e609d8207723cc
Darshan <thorboledarshan@gmail.com>
2026-07-06 08:51:26 +0530

    PC

---
+++ Java_Programming/Practice/program175.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class program175
+{ 
+  public static void Display(int iNo)
+  {
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+      System.out.print("*"+iCnt+"*\t");                   
+    }
+    System.out.println();
+  }
+
+  public static void main (String A[])
+  {
+    Scanner sobj = new Scanner(System.in);
+    int iValue = 0;
+
+    System.out.println("Enter number of elements : ");
+    iValue = sobj.nextInt();
+
+    Display(iValue);
+  }
+}
