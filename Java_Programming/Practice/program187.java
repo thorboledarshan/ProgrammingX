6240bf590d1cd33b1a3c153b7a4fa127decf95ee
Darshan <thorboledarshan@gmail.com>
2026-07-07 10:15:10 +0530

    PC

---
+++ Java_Programming/Practice/program187.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class program187
+{ 
+  public static void Display(int iNo)
+  {
+    int iCnt = 0;
+    char Ch = '\0';
+    char Ch2 = '\0';
+    for(iCnt = 1, Ch = 'A',Ch2 = 'a'; iCnt <= iNo; iCnt++, Ch++,Ch2++)
+    {
+      if(Ch % 2 == 0)
+      {
+       System.out.print(Ch2+"\t");
+      }
+      else 
+      {
+        System.out.print(Ch+"\t");
+      }
+    }
+    System.out.println();
+  }
+
+  public static void main (String A[])
+  {
+    Scanner sobj = new Scanner(System.in);
+    int iValue = 0;
+
+    System.out.println("Enter number of elements : ");
+    iValue = sobj.nextInt();
+
+    Display(iValue);
+  }
+}
