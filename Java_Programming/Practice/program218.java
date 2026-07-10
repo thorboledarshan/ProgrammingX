32496233a233dfd0218dbc2b91fe1716a75dc836
Darshan <thorboledarshan@gmail.com>
2026-07-10 23:30:33 +0530

    PC

---
+++ Java_Programming/Practice/program218.java
@@ -0,0 +1,74 @@
+/*
+    iRow = 4
+    iCol = 4
+
+    % # # #
+    % % # #
+    % % % #
+    % % % %        
+    
+    
+    trick:
+         (j)
+      1   2  3  4           1  2  3  4    
+(i) 1 11 12 13 14        1
+    2 21 22 23 24        2
+    3 31 32 33 34        3
+    4 41 42 43 44        4
+*/
+
+import java.util.*;
+
+class Pattern
+{
+  public void Display(int iRow, int iCol)
+  {
+    int i = 0;
+    int j = 0;
+
+    //Filter for diagonal pattern
+    if(iRow != iCol)
+    {
+      System.out.println("Inalid Parameters");
+      System.out.println("Number of rows and column should be same");
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        
+      for(j = 1; j <= iCol; j++)
+      {
+        if(i >= j)                    
+        {
+          System.out.print("%\t");
+        }
+        else
+        {
+          System.out.print("#\t");
+        }
+      }
+      System.out.println();
+
+    }
+
+  }
+}
+
+class program218  
+{
+    public static void main(String A[])
+    {
+       Scanner sobj = new Scanner(System.in);
+       int iValue1 = 0;
+       int iValue2 = 0;
+
+       System.out.println("Enter number of Rows : ");
+       iValue1 = sobj.nextInt();
+
+       System.out.println("Enter number of Columns : ");
+       iValue2 = sobj.nextInt();
+
+       Pattern pobj = new Pattern();
+       pobj.Display(iValue1,iValue2);
+    }
+}
