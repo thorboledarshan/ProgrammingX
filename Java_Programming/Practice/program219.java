4d2e650c7d9326c1448310e8836a71987bf687c2
Darshan <thorboledarshan@gmail.com>
2026-07-10 23:30:50 +0530

    PC

---
+++ Java_Programming/Practice/program219.java
@@ -0,0 +1,71 @@
+/*
+    iRow = 4
+    iCol = 4
+
+     *             @ # # #
+     * *           % @ # #
+     * * *         % % @ #
+     * * * *       % % % @
+    
+    
+    trick:
+         (j)
+      1   2  3  4           1  2  3  4    
+(i) 1 11 12 13 14        1
+    2 21 22 23 24        2
+    3 31 32 33 34        3
+    4 41 42 43 44        4
+*/
+
+import java.util.*;
+
+class Pattern
+{
+  public void Display(int iRow, int iCol)
+  {
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        
+      for(j = 1; j <= iCol; j++)
+      {
+        if(i == j)                    
+        {
+          System.out.print("@\t");
+        }
+        else if (i > j)
+        {
+          System.out.print("%\t");
+        }
+        else 
+        {
+          System.out.print("#\t");
+        }
+      }
+      System.out.println();
+
+    }
+
+  }
+}
+
+class program219  
+{
+    public static void main(String A[])
+    {
+       Scanner sobj = new Scanner(System.in);
+       int iValue1 = 0;
+       int iValue2 = 0;
+
+       System.out.println("Enter number of Rows : ");
+       iValue1 = sobj.nextInt();
+
+       System.out.println("Enter number of Columns : ");
+       iValue2 = sobj.nextInt();
+
+       Pattern pobj = new Pattern();
+       pobj.Display(iValue1,iValue2);
+    }
+}
