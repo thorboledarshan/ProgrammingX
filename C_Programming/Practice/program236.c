46cd5f7d7428ec8b716ae2ab9e9f0b90dd43e790
Darshan <thorboledarshan@gmail.com>
2026-07-17 14:38:23 +0530

    PC

---
+++ C_Programming/Practice/program236.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main()
+{
+    char *str = "Ganesh";   //char str[] = "Ganesh";
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+    return 0;   
+}
---
+++ C_Programming/Practice/program237.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+    char *str = "Ganesh";   //char str[] = "Ganesh";
+
+    while(*str != '\0')
+    {
+        printf("%c\n",*str);
+        str++;
+    }
+
+    
+    return 0;   
+}
