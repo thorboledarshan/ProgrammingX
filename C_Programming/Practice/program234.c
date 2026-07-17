7ea4685d48bed5c7a6260d5e798fc633044dcd85
Darshan <thorboledarshan@gmail.com>
2026-07-17 14:38:06 +0530

    PC

---
+++ C_Programming/Practice/program234.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[]  = "Jay Ganesh";
+    int iRet = 0;
+    iRet = strlen(str);
+    printf("Length of string is : %d\n",iRet);
+    
+    return 0;
+}
