9f551ab04e4a7a8670fbbf040b713690808a2696
Darshan <thorboledarshan@gmail.com>
2026-07-18 09:21:08 +0530

    PC

---
+++ C_Programming/Practice/program245.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+void Display(char *str)
+{
+    printf("%c\n",*str);
+    str++;
+    printf("%c\n",*str);
+    str++;
+    printf("%c\n",*str);
+    str++;
+}
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s",Arr);
+
+    Display(Arr);
+
+    return 0;   
+}
