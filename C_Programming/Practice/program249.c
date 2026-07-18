31ca8611f8b40eb6a31d4eec58fb9880640afaa5
Darshan <thorboledarshan@gmail.com>
2026-07-18 09:21:53 +0530

    PC

---
+++ C_Programming/Practice/program249.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int strlenX(const char *str) 
+{
+    int iCount = 0;
+    while(*str != '\0')
+    {
+        iCount++;
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = strlenX(Arr);
+    printf("Length of string is : %d\n",iRet);
+
+    return 0;   
+}
