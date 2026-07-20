1d7ed8023061cbe621371dd3213deff0ac887fca
Darshan <thorboledarshan@gmail.com>
2026-07-20 17:25:37 +0530

    PC

---
+++ C_Programming/Practice/program255.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+int CountSmall(const char *str) 
+{
+    int iCount = 0;
+    while(*str != 0)
+    {
+        if((*str >= 97 ) && (*str <= 122 )) //Bad practice
+        {
+         iCount++;
+        }
+
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = CountSmall(Arr);
+    printf("Frequency  is : %d\n",iRet);
+
+    return 0;   
+}
