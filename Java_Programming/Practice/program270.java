26009a464a9377905de7d0b71d6b6a1bbbc79276
Darshan <thorboledarshan@gmail.com>
2026-07-21 19:59:48 +0530

    PC

---
+++ Java_Programming/Practice/program270.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class StringX
+{
+  public int CountCapital(String str)
+  {
+    int iCount = 0;
+    int i = 0;
+
+    for(i = 0; i < str.length(); i++)
+    {
+      if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+      {
+        iCount++;
+      }
+    }
+    return iCount;
+  }
+}
+class program270
+{
+    public static void main(String A[])
+    {
+      Scanner sobj = new Scanner(System.in);
+      String data = null;
+      StringX strobj = new StringX();
+      
+
+      System.out.println("Enter String : ");
+      data = sobj.nextLine();
+      
+      int iRet = 0;
+      iRet = strobj.CountCapital(data);
+      System.out.println("Count of capital letter is : "+iRet);
+
+    }
+}
