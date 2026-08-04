0c0b32bab36558cec4d23041cb471bea53cab552
Darshan <thorboledarshan@gmail.com>
2026-08-04 09:24:49 +0530

    CP

---
+++ C_Programming/Competative/43.2CountSmall.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+
+#define BUFFER_SIZE 1024
+
+int CountSmall(char FileName[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int iRet = 0;
+    int fd = 0;
+    int iCount = 0;
+    int i = 0;
+
+    fd = open(FileName,O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file \n");
+        return -1;
+    }
+   
+    while((iRet = read(fd,Buffer,sizeof(Buffer)))!= 0)
+    {
+        for (i = 0; i < iRet; i++)
+        {
+            if((Buffer[i] >= 'a') && (Buffer[i] <= 'z'))
+            {
+                iCount++;
+            }
+        }
+        memset(Buffer,'\0',sizeof(Buffer));
+    }
+    close(fd);
+    
+    return iCount;
+    
+}
+int main()
+{
+    char Fname[30] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter File Name :\n");
+    scanf("%[^'\n']s",Fname);
+
+    iRet = CountSmall(Fname);
+    printf("Number of small letters are : %d\n",iRet);
+
+    return 0;
+}
