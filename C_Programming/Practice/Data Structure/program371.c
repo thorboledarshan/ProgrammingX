fcb0aede217197b331ee8ae95dfc6aea757eddfd
Darshan <thorboledarshan@gmail.com>
2026-08-16 19:10:23 +0530

    PC

---
+++ C_Programming/Practice/Data Structure/program371.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void Display(PNODE first)
+{}
+
+int Count(PNODE first)
+{return 0;}
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL) //LL is empty
+    {
+        *first = newn;
+    }
+    else               //LL contains at least one node
+    {
+       newn->next = *first;
+       *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+     PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL) //LL is empty
+    {
+        *first = newn;
+    }
+    else               //LL contains at least one node
+    {
+       
+    }
+}
+
+void InsertAtPos(PPNODE first, int ino, int iPos)
+{}
+
+void DeleteFirst(PPNODE first)
+{}
+
+void Deletelast(PPNODE first)
+{}
+
+void DeleteAtPos(PPNODE first, int iPos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+    
+    InsertFirst(&head,101);
+    InsertFirst(&head,51);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    
+    return 0;
+}
