110c9db7dc525272af6b6e4ae21cdb8fbc5ca14c
Darshan <thorboledarshan@gmail.com>
2026-08-25 13:35:17 +0530

    PC

---
+++ C++_Programming/Practice/DS/program428.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+
+class SinglyCL
+{
+    private:
+        PNODE first;
+        PNODE last;
+        int iCount;
+
+    public:
+        SinglyCL();
+
+};
+
+SinglyCL :: SinglyCL()
+{
+    cout<<"Inside constructor\n";
+    this->first = NULL;
+    this->last = NULL;
+    this->iCount = 0;
+}
+
+int main()
+{
+    SinglyCL sobj;
+    return 0;
+}
