b5b59eeaa34a5ec36f848389127ca6e9b140c531
Darshan <thorboledarshan@gmail.com>
2026-09-20 18:53:10 +0530

    Generic Programs

---
+++ C++_Programming/Practice/Generic_Programs/program477.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+float Maximum(float No1, float No2)
+{
+    float Ans;
+
+    (No1 > No2) ? Ans =  No1 : Ans =  No2;   //Terniary operator
+    
+    return Ans;
+}
+
+int main()
+{
+    cout<<Maximum(11.5f,21.2f)<<"\n";
+    return 0;
+}
