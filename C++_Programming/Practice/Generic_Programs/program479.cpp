dfdf171ba7a00245d609abf6b2cb465a26f50141
Darshan <thorboledarshan@gmail.com>
2026-09-20 18:53:37 +0530

    Generic Programs

---
+++ C++_Programming/Practice/Generic_Programs/program479.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+template <class X>
+
+X Maximum(X No1, X No2)
+{
+    X Ans;
+    if(No1 > No2)
+    {
+        Ans =  No1;
+    }
+    else 
+    {
+        Ans =  No2;
+    } 
+    return Ans;
+}
+
+int main()
+{
+    cout<<Maximum(11.5f,21.2f)<<"\n";
+    cout<<Maximum(11.5,21.2)<<"\n";
+    cout<<Maximum(11,21)<<"\n";
+    return 0;
+}
