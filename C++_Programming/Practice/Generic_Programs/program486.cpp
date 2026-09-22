710f67ae8889a3d3484b7dd3ec44ec1c3c8af283
Darshan <thorboledarshan@gmail.com>
2026-09-22 09:39:04 +0530

    Generic programs

---
+++ C++_Programming/Practice/Generic_Programs/program486.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+        int * Arr;
+        int Size;
+
+        ArrayX(int No)
+        {
+            Size = No;
+            Arr = new int[Size];
+        }
+
+        ~ArrayX()
+        {
+            delete []Arr;
+        }
+
+        void Accept()
+        {
+            int i = 0;
+
+            cout<<"Enter the elements";
+
+            for(i = 0; i < Size; i++)
+            {
+                cin>>Arr[i];
+            }
+        }
+
+        void Display()
+        {
+            int i = 0;
+
+            cout<<"Elements of the array are";
+
+            for(i = 0; i < Size; i++)
+            {
+                cout<<Arr[i]<<"\n";
+            }
+        }
+};
+
+
+int main()
+{
+    ArrayX aobj(5);
+    aobj.Accept();
+    aobj.Display();
+    return 0;
+}
