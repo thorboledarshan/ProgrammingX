f5bb7e3c835fee08dbe8d4dfad0f2c7be14c9d63
Darshan <thorboledarshan@gmail.com>
2026-09-23 19:56:31 +0530

    Delete afterwards

---
+++ Java_Programming/Practice/program654.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class program654
+{
+    public static void main(String A[])
+    {
+        File fobj = new File("Demo.txt");
+
+        fobj.createNewFile();  //Exception
+
+    }
+}
