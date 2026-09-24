91d3e83e3e86660642407a614bff1d1206e359ef
Darshan <thorboledarshan@gmail.com>
2026-09-24 15:31:09 +0530

    DS

---
+++ C_Programming/Practice/Data Structure/program507.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+void Display()
+{
+    int i = 1;
+
+    while(1)
+    {
+        printf("Jay Ganesh... %d \n",i);
+        i++;
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
