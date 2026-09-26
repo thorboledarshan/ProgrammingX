3ab3eaba3373137de6d6571cbfbf4cc56c5cd51c
Darshan <thorboledarshan@gmail.com>
2026-09-26 11:32:31 +0530

    D S

---
+++ C_Programming/Practice/Data Structure/program522.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+// input: 4
+// output: 1 + 2 + 3 + 4 = 10
+
+
+int Summation(int iNo)
+{
+    int iSum = 0;
+    int i = 0;
+    i = 1;
+    while( i <= iNo)
+    {
+        iSum = iSum + i;
+        i++;
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Frequency : \n");
+    scanf("%d",&iValue);
+
+    iRet =Summation(iValue);
+    printf("Summation is : %d\n",iRet);
+    
+    return 0;
+}
