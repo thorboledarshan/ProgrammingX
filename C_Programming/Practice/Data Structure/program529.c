f4387e927f60653d318f8a996b6f3beb3fd47574
Darshan <thorboledarshan@gmail.com>
2026-09-26 11:33:45 +0530

    D S

---
+++ C_Programming/Practice/Data Structure/program529.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+// input: 4!
+// output: 4 * 3 * 2 * 1= 10
+
+typedef unsigned long int ULONG;
+
+ULONG Factorial(int iNo)
+{
+    ULONG iFact = 1;
+    
+    while(iNo != 0)
+    {
+        iFact = iFact * iNo;
+        iNo--;
+    }
+    return iFact;
+}
+
+int main()
+{
+    int iValue = 0;
+    ULONG iRet = 0;
+
+    printf("Enter Frequency : \n");
+    scanf("%d",&iValue);
+
+    iRet =Factorial(iValue);
+    printf("Factorial is : %lu\n",iRet);
+    
+    return 0;
+}
